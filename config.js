d23b13cf4ef838811ba2d8faec16c4364d27635c
Daniel-Sogbey <mathematics06physics@gmail.com>
2023-06-01 02:29:40 +0000

    config node env

--- config.js
+++ config.js
@@ -3,16 +3,32 @@
  */
 
 //Container for all the environments
-var environment = {};
+var environments = {};
 
 // Staging  (default) environment
-environment.staging = {
+environments.staging = {
   port: 3000,
   envName: "staging",
 };
 
 //Production environment
-environment.production = {
-  port: 5000,
+environments.production = {
+  port: 4000,
   envName: "production",
 };
+
+//Determine which environment was passed in as a command-line argument
+var currentEnvironment =
+  typeof process.env.NODE_ENV == "string"
+    ? process.env.NODE_ENV.toLocaleLowerCase()
+    : "";
+
+//Check that the current environment is one of the environments above, if not default to staging
+var environmentToExport =
+  typeof environments[currentEnvironment] == "object"
+    ? environments[currentEnvironment]
+    : environments.staging;
+
+//Export the module
+
+module.exports = environmentToExport;
